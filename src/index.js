384e42935153113dbc53b097bfd76f2daddc1a05
Mali Michael <malimccalla@gmail.com>
2017-11-23 14:13:04 +0000

    Split express and ssr logic

--- src/index.js
+++ src/index.js
@@ -1,27 +1,12 @@
 // @flow
 import express from 'express'
-import React from 'react'
-import { renderToString } from 'react-dom/server'
+import renderer from './helpers/renderer'
 
 const app = express()
 
-const Home = require('./client/components/Home').default
-
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  const content = renderToString(<Home />)
-
-  const html = `
-    <html>
-      <head></head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-  `
-
-  res.send(html)
+  res.send(renderer())
 })
 
 app.listen(3000, () => {
