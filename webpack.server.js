2f0bb6858be60c6f5432be47ff0c599db9e6f742
Mali Michael <malimccalla@gmail.com>
2017-11-23 13:58:59 +0000

    Extract common webpack behaviours

--- webpack.server.js
+++ webpack.server.js
@@ -1,6 +1,8 @@
 const path = require('path')
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base')
 
-module.exports = {
+const config = {
   // Inform webpack we are building a bundle for nodeJs
   // rather than for the browser
   target: 'node',
@@ -13,23 +15,6 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-
-  // Tell webpack to run babel on every file it runs through
-  module: {
-    rules: [
-      {
-        test: /\.js?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: [
-            'react',
-            'stage-0',
-            'flow',
-            ['env', { tagets: { browsers: ['last 2 versions'] } }],
-          ],
-        },
-      },
-    ],
-  },
 }
+
+module.exports = merge(baseConfig, config)
